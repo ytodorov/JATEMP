005e0612b43a64a400ac007267795542be3f5b9b
Yordan Todorov <yordan.todorov@mentormate.bg>
2019-05-10 17:49:18 +0300

    BaseEntity

---
+++ JA.FinancePark/src/JA.FinancePark.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using JA.FinancePark.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JA.FinancePark.Core.Entities
+{
+    public class BaseEntity
+    {
+        public override string ToString()
+        {
+            string result = TypeHelper.GetStringRepresentation(this);
+            return result;
+        }
+    }
+}
---
+++ JA.FinancePark/src/JA.FinancePark.Core/Helpers/TypeHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace JA.FinancePark.Core.Helpers
+{
+    public static class TypeHelper
+    {
+        public static string GetStringRepresentation(object instance)
+        {
+            Type type = instance.GetType();
+
+            List<PropertyInfo> props = type.GetProperties().OrderBy(o => o.Name).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (PropertyInfo prop in props)
+            {
+                if (prop.CanRead && (prop.PropertyType.IsPrimitive ||
+                    prop.PropertyType == typeof(string) ||
+                    prop.PropertyType == typeof(int) ||
+                    prop.PropertyType == typeof(long) ||
+                    prop.PropertyType == typeof(bool) ||
+                    prop.PropertyType == typeof(bool?)))
+                {
+                    object val = prop.GetValue(instance);
+                    if (val != null)
+                    {
+                        sb.Append($"{prop.Name}:{val} | ");
+                    }
+                }
+            }
+
+            string result = sb.ToString();
+
+            return result;
+        }
+    }
+}
